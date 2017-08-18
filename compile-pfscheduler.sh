572e0e1d6f6986572dc3d32f9bcd3b271af2d78b
nelsounet <nelsounet@afrostream.tv>
2017-08-18 15:13:32 +0200

    fix docker composition

--- compile-pfscheduler.sh
+++ compile-pfscheduler.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-export GOPATH=/go/src/pfscheduler
+export GOPATH=/go/
 export GOOS=linux
 export GOARCH=amd64
-export GOBIN=/go/app
+export GOBIN=/go/app/
 
 cd /go/src/pfscheduler
 go get -v
+go install
